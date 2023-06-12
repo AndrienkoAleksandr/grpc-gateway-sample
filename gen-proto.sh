33b9ee0fdc1c1dad20386119b041aae1de59f0f0
Oleksandr Andriienko <oandriie@redhat.com>
2023-06-12 20:57:31 +0300

    Add gateway stubs.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- gen-proto.sh
+++ gen-proto.sh
@@ -4,3 +4,9 @@ protoc -I . \
     --go_out ./gen/go/ --go_opt paths=source_relative \
     --go-grpc_out ./gen/go/ --go-grpc_opt paths=source_relative \
     your/service/v1/your_service.proto
+
+protoc -I . --grpc-gateway_out ./gen/go \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt generate_unbound_methods=true \
+    your/service/v1/your_service.proto
