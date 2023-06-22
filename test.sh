3a8ba528807a069b4f1245d9c9e774b1950b20e8
Oleksandr Andriienko <oandriie@redhat.com>
2023-06-22 17:52:43 +0300

    Test.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- test.sh
+++ test.sh
@@ -9,3 +9,5 @@ grpc_cli call localhost:50051  your.service.v1.YourService.Echo 'value: "test"'
 echo "=> curl gRPC gateway"
 curl -X POST localhost:3001/your.service.v1.YourService/Echo -d '{"value": "echo"}'
 echo
+
+curl -X POST -k -v https://grpc-gateway-sample-oandriie-tenant.apps.ci-ln-qpxtgrt-72292.gcp-2.ci.openshift.org/your.service.v1.YourService/Echo -d '{"value": "echo"}'
